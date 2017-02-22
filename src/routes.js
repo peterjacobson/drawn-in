a0ef4410e352497990bd9c57b5cb2067cbc191ca
Peter Jacobson <peterjacobson.nz@gmail.com>
2017-02-22 13:35:33 +1300

    remove prematurely loading ga-react module

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,12 @@
 import React from "react"
 import { Route } from "react-router"
 import { PageContainer as PhenomicPageContainer } from "phenomic"
-import ReactGA from "react-ga"
 
 import AppContainer from "./AppContainer"
 import Page from "./layouts/Page"
 import PageError from "./layouts/PageError"
 import Homepage from "./layouts/Homepage"
 import Post from "./layouts/Post"
-
-ReactGA.initialize('UA-72072231-2')
 
 const PageContainer = (props) => (
   <PhenomicPageContainer
@@ -23,13 +20,8 @@ const PageContainer = (props) => (
   />
 )
 
-function logPageView() {
-  // ReactGA.set({ page: window.location.pathname });
-  // ReactGA.pageview(window.location.pathname);
-}
-
 export default (
   <Route component={ AppContainer }>
-    <Route path="*" component={ PageContainer } onUpdate={logPageView}/>
+    <Route path="*" component={ PageContainer }/>
   </Route>
 )
