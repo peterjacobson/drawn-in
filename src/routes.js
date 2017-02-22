f0e1c66cb76300004b55c714a04247a9e6da25ad
Peter Jacobson <peterjacobson.nz@gmail.com>
2017-02-22 13:29:14 +1300

    half implement ga-react

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { Route } from "react-router"
 import { PageContainer as PhenomicPageContainer } from "phenomic"
+import ReactGA from "react-ga"
 
 import AppContainer from "./AppContainer"
 import Page from "./layouts/Page"
 import PageError from "./layouts/PageError"
 import Homepage from "./layouts/Homepage"
 import Post from "./layouts/Post"
+
+ReactGA.initialize('UA-72072231-2')
 
 const PageContainer = (props) => (
   <PhenomicPageContainer
@@ -20,8 +23,13 @@ const PageContainer = (props) => (
   />
 )
 
+function logPageView() {
+  // ReactGA.set({ page: window.location.pathname });
+  // ReactGA.pageview(window.location.pathname);
+}
+
 export default (
   <Route component={ AppContainer }>
-    <Route path="*" component={ PageContainer } />
+    <Route path="*" component={ PageContainer } onUpdate={logPageView}/>
   </Route>
 )
